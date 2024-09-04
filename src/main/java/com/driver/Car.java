9b9267103021261801b12397e28f74940b19baa6
Minesh <54360467+mineshgaikar50@users.noreply.github.com>
2024-09-04 15:58:00 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,7 +10,14 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
+        super(name);  
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1; 
     }
 
     public void changeGear(int newGear){
