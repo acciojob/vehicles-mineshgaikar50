9e63b15344436f1d66f6e9d204b03a32f19161d1
Minesh <54360467+mineshgaikar50@users.noreply.github.com>
2024-09-04 15:56:32 +0530

    Update WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,27 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+class Boat implements WaterVehicle {
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+    public void printDetails() {
+        System.out.println("Boat Name: " + name);
+        System.out.println("Capacity: " + capacity);
+    }
+}
